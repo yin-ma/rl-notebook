825870d5a6807eaef151fd470559f849f46d325a
unknown <yinma2448@gmail.com>
2025-03-11 15:11:54 +0800

    init proj

---
+++ qLearning/main.py
@@ -0,0 +1,87 @@
+import gymnasium as gym
+import numpy as np
+from tqdm import tqdm
+
+#env = gym.make('CliffWalking-v0')
+env = gym.make('Taxi-v3')
+#env = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=False)
+
+#env_test = gym.make('CliffWalking-v0', render_mode="human")
+env_test = gym.make('Taxi-v3', render_mode="human")
+#env_test = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=False, render_mode="human")
+
+
+observation_space = env.observation_space.n
+action_space = env.action_space.n
+
+print("observation space: ", observation_space)
+print("action space: ", action_space)
+
+# Q-table
+Q = np.zeros((observation_space, action_space))
+
+# parameters
+epsilon = 1.0
+epsilon_decay = 0.999
+epsilon_min = 0.05
+gamma = 0.999
+alpha = 0.1
+
+epoch = 1000
+max_step = 200
+
+
+def train(env):
+    print("training...")
+    global epsilon
+    for i in tqdm(range(epoch)):
+        observation, info = env.reset()
+        terminated = False
+        truncated = False
+        curr_step = max_step
+        while True:
+            action = 0
+            # epsilon greedy
+            if np.random.random() > epsilon:
+                action = np.argmax(Q[observation, :])
+            else:
+                action = env.action_space.sample()
+
+            new_observation, reward, terminated, truncated, info = env.step(action)
+
+            # Q_old = Q_old + alpha * (R + gamma * Q_new - Q_old)
+            Q[observation, action] = Q[observation, action] \
+                + alpha * (reward + gamma * np.max(Q[new_observation, :]) - Q[observation, action])
+
+            observation = new_observation
+
+            if terminated or truncated or curr_step <= 0:
+                break
+
+            curr_step -= 1
+    epsilon = epsilon_decay * epsilon
+    epsilon = max(epsilon_min, epsilon)
+
+
+def test(env):
+    observation, info = env.reset()
+    terminated = False
+    truncated = False
+    curr_step = max_step
+
+    while True:
+        env.render()
+        action = np.argmax(Q[observation, :])
+
+        new_observation, reward, terminated, truncated, info = env.step(action)
+        observation = new_observation
+
+        if terminated or truncated or curr_step <= 0:
+            break
+        curr_step -= 1
+    env.close()
+
+
+if __name__ == "__main__":
+    train(env)
+    test(env_test)
